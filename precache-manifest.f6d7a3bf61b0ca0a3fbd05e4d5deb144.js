9c5998f4508822df1ccde3882876262a822a12f6
Mikhail Karpov <mihanikk@gmail.com>
2019-02-26 18:00:21 +0300

    Updates

---
+++ precache-manifest.f6d7a3bf61b0ca0a3fbd05e4d5deb144.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9324f73cd31d204a558c",
+    "url": "/bikes-search/static/js/runtime~main.9324f73c.js"
+  },
+  {
+    "revision": "51bbbabc5e2fe0f94318",
+    "url": "/bikes-search/static/js/main.51bbbabc.chunk.js"
+  },
+  {
+    "revision": "6d6c00bc07ec1a4408ed",
+    "url": "/bikes-search/static/js/2.6d6c00bc.chunk.js"
+  },
+  {
+    "revision": "51bbbabc5e2fe0f94318",
+    "url": "/bikes-search/static/css/main.0e95837f.chunk.css"
+  },
+  {
+    "revision": "789e49bfad71a1a0be49f28043ac6299",
+    "url": "/bikes-search/index.html"
+  }
+];
